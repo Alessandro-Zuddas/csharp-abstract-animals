cdfd5e589a188166a3f0622d5045867c5651e5a7
Alessandro-Zuddas <alessandro.zuddas1999@gmail.com>
2023-03-23 16:50:32 +0100

    Add classe pulcinella di mare con implementazione interfacce multiple

--- Program.cs
+++ Program.cs
@@ -5,23 +5,29 @@ var dog = new Cane();
 var passero = new Passerotto();
 var eagle = new Aquila();
 var dolphin = new Delfino();
+var seaBird = new PulcinellaDiMare();
 
 dog.Verso();
 passero.Verso();
 eagle.Verso();
 dolphin.Verso();
+seaBird.Verso();
 
 dog.Mangia();
 passero.Mangia();
 eagle.Mangia();
 dolphin.Mangia();
+seaBird.Mangia();
 
 dog.Dormi();
 passero.Dormi();   
 eagle.Dormi();
 dolphin.Dormi();
+seaBird.Dormi();
 
 dog.Nuota();
 passero.Vola();
 eagle.Vola();
 dolphin.Nuota();
+seaBird.Vola();
+seaBird.Nuota();
---
+++ PulcinellaDiMare.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_abstract_animals
+{
+    public class PulcinellaDiMare : Animals, IVolante, INuotante
+    {
+        public override void Mangia()
+        {
+            Console.WriteLine("Pesci e insetti");
+        }
+
+        public override void Verso()
+        {
+            Console.WriteLine("Cip cip cip!");
+        }
+
+        public void Vola()
+        {
+            Console.WriteLine("La pulcinella di mare vola!");
+        }
+
+        public void Nuota()
+        {
+            Console.WriteLine("La pulcinella di mare nuota!");
+        }
+    }
+}
