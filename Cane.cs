c4ade9c21720669a82cf56ed815b037ef039629a
Alessandro-Zuddas <alessandro.zuddas1999@gmail.com>
2023-03-23 16:33:49 +0100

    Add classi animali e istanze testate

---
+++ Cane.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_abstract_animals
+{
+    public class Cane : Animals
+    {
+        public override void Mangia()
+        {
+            Console.WriteLine("Crocchette");
+        }
+
+        public override void Verso()
+        {
+            Console.WriteLine("Bau!");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,22 @@
 ﻿
+using csharp_abstract_animals;
+
+var dog = new Cane();
+var passero = new Passerotto();
+var eagle = new Aquila();
+var dolphin = new Delfino();
+
+dog.Verso();
+passero.Verso();
+eagle.Verso();
+dolphin.Verso();
+
+dog.Mangia();
+passero.Mangia();
+eagle.Mangia();
+dolphin.Mangia();
+
+dog.Dormi();
+passero.Dormi();   
+eagle.Dormi();
+dolphin.Dormi();
