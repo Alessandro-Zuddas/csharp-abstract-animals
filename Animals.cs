ac27ea9992ceb39a245490da126365a993df3f7b
Alessandro-Zuddas <alessandro.zuddas1999@gmail.com>
2023-03-23 16:23:06 +0100

    Add classe astratta animals

---
+++ Animals.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_abstract_animals
+{
+    public abstract class Animals
+    {
+        public void Dormi()
+        {
+            Console.WriteLine("Zzzzzz...");
+        }
+
+        public abstract void Verso();
+
+        public abstract void Mangia();
+    }
+}
